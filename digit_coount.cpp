23cba0f65920a3963d22535918a225d535a263a2
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2020-11-05 22:11:31 +0600

    Add files via upload

---
+++ digit_coount.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+
+int main()
+{
+    int number ;
+
+    cin>>number ;
+
+    float a = log10(number);
+    float b = floor(a) + 1 ;
+    cout<<b<<endl;
+
+
+
+
+    return 0 ;
+}
